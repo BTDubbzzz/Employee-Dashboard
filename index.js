5ee124211c656900a0262f4e1609984501ee0bd3
BTDubbzzz <btdubbzzz@gmail.com>
2021-05-21 00:06:52 -0500

    refactored for cleaner and shorter/DRYer code

--- index.js
+++ index.js
@@ -1,114 +1,101 @@
-const inquirer = require('inquirer')
-const fs = require('fs')
-const HTML = require('./src/generateHTML')
-
-const Manager = require('./lib/Manager')
-const Engineer = require('./lib/Engineer')
-const Intern = require('./lib/Intern')
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const HTML = require('./src/generateHTML');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 const employeeQuestions = [
-    {
-        type: 'input',
-        message: 'Pleasee enter employee name: ',
-        name: 'name',
-    },
-    {
-        type: 'input',
-        message: 'Pleasee enter employee id: ',
-        name: 'id',
-    },
-    {
-        type: 'input',
-        message: 'Pleasee enter employee email: ',
-        name: 'email',
-    },
-]
+	{
+		type: 'input',
+		message: 'Pleasee enter employee name: ',
+		name: 'name',
+	},
+	{
+		type: 'input',
+		message: 'Pleasee enter employee id: ',
+		name: 'id',
+	},
+	{
+		type: 'input',
+		message: 'Pleasee enter employee email: ',
+		name: 'email',
+	},
+];
 const anotherEmployeeQuestion = [
-    {
-        type: 'list',
-        message: 'Would you like to add another employee? Select from the following: ',
-        name: 'nextEmployee',
-        choices: [
-            'Intern',
-            'Engineer',
-            'No more employees (end program)'
-        ]
-    }
-]
-
+	{
+		type: 'list',
+		message:
+			'Would you like to add another employee? Select from the following: ',
+		name: 'nextEmployee',
+		choices: ['Intern', 'Engineer', 'No more employees (end program)'],
+	},
+];
 const managerQuestions = [
-    {
-        type: 'input',
-        message: 'Please enter manager office number: ',
-        name: 'officeNumber'
-    }
-]
-
+	{
+		type: 'input',
+		message: 'Please enter manager office number: ',
+		name: 'officeNumber',
+	},
+];
 const internQuestions = [
-    {
-        type: 'input',
-        message: 'Please enter intern school name: ',
-        name: 'school'
-    }
-]
-
+	{
+		type: 'input',
+		message: 'Please enter intern school name: ',
+		name: 'school',
+	},
+];
 const engineerQuestions = [
-    {
-        type: 'input',
-        message: 'Please enter engineer gitHub username: ',
-        name: 'gitHub'
-    }
-]
-
-function getEmployeeInfo(role = 'manager') {
-    if (role === 'no more employees (end program)') {
-        fs.appendFile('./dist/team.html', `\n${HTML.generateBottomHTML()}`, (err) => {})
-        return
-    }
-    if (role === 'manager') {
-        //TODO: generate top html
-        fs.writeFile('./dist/team.html', HTML.generateTopHTML(), (err) => {})
-        inquirer
-            .prompt([].concat(employeeQuestions, managerQuestions, anotherEmployeeQuestion))
-            .then(function (response) {
-                const  { name, id, email, officeNumber } = response
-                const newEmployee = new Manager(name, id, email, officeNumber)
-                fs.appendFile('./dist/team.html', `\n${HTML.generateManagerHTML(newEmployee.name, newEmployee.id, newEmployee.email, newEmployee.officeNumber)}`, (err) => {})
-                return response.nextEmployee
-            })
-            .then(function (nextEmployee) {
-                getEmployeeInfo(nextEmployee.toLowerCase())
-            })
-    }
-    if (role === 'engineer') {
-        inquirer
-            .prompt([].concat(employeeQuestions, engineerQuestions, anotherEmployeeQuestion))
-            .then(function (response) {
-                const  { name, id, email, gitHub } = response
-                const newEmployee = new Engineer(name, id, email, gitHub)
-                fs.appendFile('./dist/team.html', `\n${HTML.generateEngineerHTML(newEmployee.name, newEmployee.id, newEmployee.email, newEmployee.gitHub)}`, (err) => {})
-                return response.nextEmployee
-            })
-            .then(function (nextEmployee) {
-                getEmployeeInfo(nextEmployee.toLowerCase())
-            })
-    }
-    if (role === 'intern') {
-        inquirer
-            .prompt([].concat(employeeQuestions, internQuestions, anotherEmployeeQuestion))
-            .then(function (response) {
-                const  { name, id, email, school } = response
-                const newEmployee = new Intern(name, id, email, school)
-                fs.appendFile('./dist/team.html', `\n${HTML.generateInternHTML(newEmployee.name, newEmployee.id, newEmployee.email, newEmployee.school)}`, (err) => {})
-                return response.nextEmployee
-            })
-            .then(function (nextEmployee) {
-                getEmployeeInfo(nextEmployee.toLowerCase())
-            })
-    }
+	{
+		type: 'input',
+		message: 'Please enter engineer gitHub username: ',
+		name: 'gitHub',
+	},
+];
+function getEmployeeInfo(role, fileType, questionsType) {
+	inquirer
+		.prompt([].concat(employeeQuestions, questionsType, anotherEmployeeQuestion))
+		.then(function (response) {
+			const { name, id, email } = response;
+			const specialProperty = Object.keys(response)[3];
+			newEmployee = new role(name, id, email, response[specialProperty]);
+			fs.appendFile(
+				'./dist/team.html',
+				`\n${fileType(
+					newEmployee.name,
+					newEmployee.id,
+					newEmployee.email,
+					newEmployee[specialProperty]
+				)}`,
+				(err) => {}
+			);
+			return response.nextEmployee;
+		})
+		.then(function (nextEmployee) {
+			getEmployeeRole(nextEmployee.toLowerCase());
+		});
+}
+function getEmployeeRole(role = 'manager') {
+	if (role === 'no more employees (end program)') {
+		fs.appendFile(
+			'./dist/team.html',
+			`\n${HTML.generateBottomHTML()}`,
+			(err) => {}
+		);
+		return;
+	}
+	if (role === 'manager') {
+		fs.writeFile('./dist/team.html', HTML.generateTopHTML(), (err) => {});
+		getEmployeeInfo(Manager, HTML.generateManagerHTML, managerQuestions);
+	}
+	if (role === 'engineer') {
+		getEmployeeInfo(Engineer, HTML.generateEngineerHTML, engineerQuestions);
+	}
+	if (role === 'intern') {
+		getEmployeeInfo(Intern, HTML.generateInternHTML, internQuestions);
+	}
 }
 function init() {
-    console.log('Please enter Manager info first')
-    getEmployeeInfo()
+	console.log('Please enter Manager info first');
+	getEmployeeRole();
 }
-init()
+init();
